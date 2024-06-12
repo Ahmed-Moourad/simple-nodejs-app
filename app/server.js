0bb966b7f85d50387b89001d2c62528eefea1068
Ahmed-Moourad <eng.ahmed.a.mourad@gmail.com>
2024-06-12 11:16:01 +0300

    update the server file

--- app/server.js
+++ app/server.js
@@ -9,6 +9,11 @@ logger.info('hello world');
 logger.info('I am DEPI- DevOps learner');
 logger.info('This is a nodejs app for testing');
 
+// Define a route for the root URL (/)
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
 app.listen(3000, function () {
     logger.info("app listening on port 3000!");
 });
